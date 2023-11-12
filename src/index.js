eade94d8901d68618f0aed2689147541e0c0cef1
kellymokeefe <128492828+kellymokeefe@users.noreply.github.com>
2023-11-12 17:08:21 +0100

    Added searchCity function with API key and url.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+function searchCity(city) {
+ let apiKey = "e7f44dtf8936b0ao9a003f375cfb3403";
+ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+ console.log(apiUrl);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#city-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
