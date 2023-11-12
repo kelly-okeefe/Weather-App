0d72b1035d3be9860680ab74d82b54825cfd9b25
kellymokeefe <128492828+kellymokeefe@users.noreply.github.com>
2023-11-12 18:17:37 +0100

    Added code to update weather description on search.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 function refreshWeather(response) {
-   let temperatureElement=document.querySelector("#temperature");
+   let temperatureElement = document.querySelector("#temperature");
    let temperature = response.data.temperature.current;
    let cityElement = document.querySelector("#city");
+   let descriptionElement = document.querySelector("#description");
    cityElement.innerHTML = response.data.city;
+   descriptionElement.innerHTML = response.data.condition.description;
    temperatureElement.innerHTML = Math.round(temperature);
-   
 }
 
 function searchCity(city) {
