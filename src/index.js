4b1736c345d7dbf477800069c2347d17dbe2fa23
kellymokeefe <128492828+kellymokeefe@users.noreply.github.com>
2023-11-12 19:08:34 +0100

    Added code to update date and time on search.

--- src/index.js
+++ src/index.js
@@ -8,14 +8,31 @@ function refreshWeather(response) {
    let windSpeed = response.data.wind.speed;
    let feelsLikeElement = document.querySelector("#feels-like");
    let feelsLike = response.data.temperature.feels_like;
+   let timeElement = document.querySelector("#date-time");
+   let date = new Date(response.data.time * 1000);
    console.log(response);
    cityElement.innerHTML = response.data.city;
+   timeElement.innerHTML = formatDate(date);
    descriptionElement.innerHTML = response.data.condition.description;
    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
    windSpeedElement.innerHTML = `${Math.round(windSpeed)} km/h`;
    temperatureElement.innerHTML = `${Math.round(temperature)}°c`;
    feelsLikeElement.innerHTML = `${Math.round(feelsLike)}°c`;
 }
+
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days [date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}, `;
+    
+}
+
 
 function searchCity(city) {
  let apiKey = "e7f44dtf8936b0ao9a003f375cfb3403";
